71a0f08adb951c3726586e3a83089669e4ca3284
loukas922 <loukas@plaud.ai>
2025-12-22 12:57:15 +0800

    fix: 增加版本前缀路由

--- api/router.go
+++ api/router.go
@@ -68,7 +68,7 @@ func InitRouter(services Services) (public http.Handler, private http.Handler) {
 	}
 
 	// myplaud - 心智幕僚邮箱相关接口
-	myplaud := publicRouter.Group("/myplaud")
+	myplaud := publicRouter.Group("/v1/myplaud")
 	{
 		myplaud.POST("/mailbox", mailboxHandler.CreateMailbox)
 		myplaud.GET("/mailbox", mailboxHandler.GetMailbox)
